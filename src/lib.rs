498cefc3d6f0ffdf31fea2cab64e4ef71cf21fd1
Ben Woodcroft <donttrustben@gmail.com>
2021-11-26 06:39:38 +1000

    GenomeQuality: Improve retrieval.

--- src/lib.rs
+++ src/lib.rs
@@ -181,7 +181,13 @@ pub struct GenomeQuality {
 
 impl CheckMResult {
     pub fn retrieve_via_fasta_path(&self, fasta_path: &str) -> Result<GenomeQuality,()> {
-        let checkm_name = std::path::Path::new(fasta_path).file_stem().unwrap().to_str().unwrap();
+        let checkm_name = std::path::Path::new(fasta_path)
+            .file_stem()
+            .expect(&format!("Failed to find file_stem for {}", fasta_path))
+            .to_str()
+            .expect(&format!("Failed to convert fasta file name to string: {}", fasta_path));
+        debug!("Retrieving checkm name {}",fasta_path);
+        debug!("{:?}",self.genome_to_quality);
         match self.genome_to_quality.get(checkm_name) {
             Some(q) => Ok(*q),
             None => Err(())
